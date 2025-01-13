a4ffe69595ac3080fa9e823296ce34c33684a9d0
Foqsz <contatovictorvinicius05@gmail.com>
2025-01-13 08:46:33 -0300

    Add project files.

---
+++ Projeto-AgendaConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Projeto_AgendaConsole;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Armazenamento de dados da agenda
+        String[] nome = new string[200];
+        String[] email = new string[200];
+
+        int tl=0; // Tamanho lógico da agenda
+    }
+}
