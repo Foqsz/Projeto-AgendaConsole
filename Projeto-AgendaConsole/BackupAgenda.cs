f5c5f071e6280717d557f6184af4b1daf55e984a
Foqsz <contatovictorvinicius05@gmail.com>
2025-01-14 10:39:11 -0300

    feat: metodo para restaurar os dados ao executar novamente a aplicação

--- Projeto-AgendaConsole/BackupAgenda.cs
+++ Projeto-AgendaConsole/BackupAgenda.cs
@@ -15,13 +15,26 @@ internal class BackupAgenda
         StreamWriter arquivo = new StreamWriter(nomeArquivo);
         for(int i = 0; i < tl; i++)
         {
-            arquivo.WriteLine("Nome: " + nome[i] + " - Email: " + email[i]); 
+            arquivo.WriteLine(nome[i] + "-" + email[i]); 
         }
         arquivo.Close();
     }  
 
     public static void RestaurarDados(ref string[] nome, ref string[] email, ref int tl)
     {
+        tl = 0;
+        int pos = 0;
 
+        StreamReader streamReader = new StreamReader(nomeArquivo);
+        string linha = streamReader.ReadLine(); 
+        while(linha != null)
+        {
+            pos = linha.IndexOf('-');
+            nome[tl] = linha.Substring(0, pos);
+            email[tl] = linha.Substring(pos + 1);
+            tl++;
+            linha = streamReader.ReadLine();
+        }
+        streamReader.Close();
     }
 }
